d0b4609925a2b4871d358d9f812735893059982c
Kay Kleinvogel <kaykleinvogel@googlemail.com>
2022-03-06 15:32:40 +0100

    feat(#1): adds the basic component for visualization

---
+++ src/components/WaveDisplay/WaveDisplay.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, Typography } from "@mui/material";
+import React from "react";
+
+const WaveDisplay = () => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography>WaveDisplay</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default WaveDisplay;
